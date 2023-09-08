c007d67d7f26db6e33bead9648adde9af710faa6
shahin569 <sahin.zaarasara@gmail.com>
2023-09-09 04:14:51 +0600

    function loaded to html

--- api.js
+++ api.js
@@ -1,5 +1,39 @@
-function dataLoad(){
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// function dataLoad(){
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(sumon => sumon.json()) //parameters name could be anything
+//       .then(shahin => console.log(shahin)) // parameters name could be anything 
+// }
+
+
+// function loadUsers(){
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(data => displayUsers(data))
+// }
+
+//     function displayUsers(data){
+
+//         for (const user of data) {
+//             console.log(user.email);            
+//         }
+
+//     }
+
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => displayData(data))
+
 }
+
+function displayData(data){
+    const ul = document.getElementById('user-list');
+    for(const user of data){
+        const li = document.createElement('li');
+        li.innerHTML = user.name + '</br>' + user.email;
+        ul.appendChild(li);        
+    }
+}
+
+
